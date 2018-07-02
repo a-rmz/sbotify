b455e9b2677da700284f6a20ed5dc8273b276ce8
a-rmz <alex.ramirez@wizeline.com>
2018-07-01 19:04:22 -0500

    :card_file_box: Update syntax for MySQL

--- migrations/sqls/20170526201851-initial-up.sql
+++ migrations/sqls/20170526201851-initial-up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE SlackCredentials (
-  team_id TEXT PRIMARY KEY,
-  team_name TEXT,
-  access_token TEXT,
-  bot_user_id TEXT,
-  bot_access_token TEXT
+  team_id VARCHAR(50) PRIMARY KEY,
+  team_name VARCHAR(50),
+  access_token VARCHAR(100),
+  bot_user_id VARCHAR(50),
+  bot_access_token VARCHAR(50)
 );
