9374c68a8b2a0eeb60e2c6f1ad0d433d2845552a
a-rmz <alex.ramirez@wizeline.com>
2018-07-01 19:00:38 -0500

    :card_file_box: Update how DB is accessed

--- src/models/SlackCredentialModel.js
+++ src/models/SlackCredentialModel.js
@@ -2,52 +2,40 @@
 
 const logger = require('../lib/logger');
 
-const sqlite = require('sqlite');
-const dbConfig = require('../config/db');
+const db = require('../lib/db');
+const { table } = require('../config/db');
 const SlackCredential = require('../schemas/SlackCredential');
 
 class SlackCredentialModel {
 
-  static save(cred: SlackCredential): void {
-    sqlite.open(dbConfig.dbPath)
-    .then(a => {
-      sqlite.run(
-        `REPLACE INTO ${dbConfig.slack.dbTable}(team_id, team_name, access_token, bot_user_id, bot_access_token)
-        VALUES("${cred.teamId}", "${cred.teamName}", "${cred.accessToken}", "${cred.botUserId}", "${cred.botAccessToken}");`
+  static save(cred: SlackCredential): any {
+    return db.then(connection =>
+      connection.query(
+        `INSERT INTO SlackCredentials (team_id, team_name, access_token, bot_user_id, bot_access_token)
+        VALUES(?, ?, ?, ?, ?)
+        ON DUPLICATE KEY UPDATE
+        team_name = ?, access_token = ?, bot_user_id = ?, bot_access_token = ?;`,
+        [cred.teamId, cred.teamName, cred.accessToken, cred.botUserId, cred.botAccessToken,
+          cred.teamName, cred.accessToken, cred.botUserId, cred.botAccessToken]
       )
       .catch(reason => logger.error(reason))
-      .then(a => {
-        sqlite.close();
-      });
-    });
+    );
   }
 
   static retrieve(teamId: string): Promise<SlackCredential> {
-    return sqlite.open(dbConfig.dbPath)
-      .then(a => {
-        return sqlite.get(
-          `SELECT * FROM ${dbConfig.slack.dbTable} WHERE team_id="${teamId}"`
-        )
-        .then(credentialResponse => {
-          const credential: SlackCredential = new SlackCredential({
-            accessToken: credentialResponse.access_token,
-            teamName: credentialResponse.team_name,
-            teamId: credentialResponse.team_id,
-            botUserId: credentialResponse.bot_user_id,
-            botAccessToken: credentialResponse.bot_access_token
-          });
+    return db.then(connection => connection.query(
+      'SELECT * FROM SlackCredentials WHERE team_id = ?',
+      [teamId]
+    ))
+    .then(([credentialResponse, _]) => {
+      const credential: SlackCredential = new SlackCredential(credentialResponse[0]);
 
-          return credential;
-        })
-        .catch(reason => {
-          logger.error(reason);
-          return new SlackCredential();
-        })
-        .then(creds => {
-          sqlite.close();
-          return creds;
-        });
-      });
+      return credential;
+    })
+    .catch(reason => {
+      logger.error(reason);
+      return new SlackCredential();
+    });
   }
 
   static remove(teamId: string): void {
